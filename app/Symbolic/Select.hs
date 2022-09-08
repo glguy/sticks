5716ea659c551b4b86d9dceabf49cef942a1f8c0
Eric Mertens <emertens@galois.com>
2022-09-08 15:26:29 -0700

    simplify

--- app/Symbolic/Select.hs
+++ app/Symbolic/Select.hs
@@ -10,7 +10,7 @@ module Symbolic.Select
   ) where
 
 import Control.Applicative
-import Control.Monad.State
+import Control.Monad (replicateM, ap)
 import Data.List (tails)
 import Data.List.NonEmpty (NonEmpty(..))
 import Ersatz
@@ -26,27 +26,20 @@ data Select a = Selected a | Choose (Select a) (Select a) Bit
 selectList :: MonadSAT s m => [a] -> m (Select a)
 selectList []     = error "selectList: empty list"
 selectList (x:xs) = select (x :| xs)
-{-# SPECIALIZE selectList :: [a] -> StateT SAT IO (Select a) #-}
 
 -- | Symbolic selection from a non-empty list of alternatives.
 select :: MonadSAT s m => NonEmpty a -> m (Select a)
 select = mergeSelects . fmap Selected
 
 mergeSelects :: MonadSAT s m => NonEmpty (Select a) -> m (Select a)
-mergeSelects (x :| xs) =
-    case xs of
-        []   -> pure x
-        y:ys -> 
-         do b  <- exists
-            zs <- reduce ys
-            mergeSelects (Choose x y b :| zs)
+mergeSelects (x :| []    ) = pure x
+mergeSelects (x :| y : zs) =
+ do b <- exists
+    mergeSelects (Choose x y b :| reduce b zs)
 
-reduce :: MonadSAT s m => [Select a] -> m [Select a]
-reduce (x1:x2:xs) =
- do b   <- exists
-    xs' <- reduce xs
-    pure (Choose x1 x2 b : xs')
-reduce xs = pure xs
+reduce :: Bit -> [Select a] -> [Select a]
+reduce b (x1:x2:xs) = Choose x1 x2 b : reduce b xs
+reduce _ xs = xs
 
 runSelect :: ChooseBit a => Select a -> a
 runSelect = \case
@@ -56,15 +49,16 @@ runSelect = \case
 instance Codec (Select a) where
     type Decoded (Select a) = a
     encode = Selected
-    decode sol = \case
-        Selected x -> pure x
-        Choose x y b ->
+    decode _ (Selected x) = pure x
+    decode sol (Choose x y b) =
          do b' <- decode sol b
             decode sol if b' then y else x
 
 instance Applicative Select where
     pure = Selected
     (<*>) = ap
+    x <* _ = x
+    _ *> x = x
 
 instance Monad Select where
     Selected x   >>= f = f x
@@ -95,10 +89,9 @@ selectPermutationN n xs
 -- It compares the internal locations of the select, not the
 -- actual values.
 unsafeUniqueSelects :: [Select a] -> Bit
-unsafeUniqueSelects ys =
-    nor [ sameSelect y z | y:zs <- tails ys, z <- zs ]
+unsafeUniqueSelects ys = and [ different y z | y:zs <- tails ys, z <- zs ]
     where
-    sameSelect (Choose f1 t1 b1) (Choose f2 t2 b2) =
-        not b1 && not b2 && sameSelect f1 f2 ||
-            b1 &&     b2 && sameSelect t1 t2
-    sameSelect _ _ = true
+    different (Choose f1 t1 b1) (Choose f2 t2 b2) =
+        (not b1 && not b2 ==> different f1 f2) &&
+        (    b1 &&     b2 ==> different t1 t2)
+    different _ _ = false
